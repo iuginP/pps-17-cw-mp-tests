715cc58c4b5a7527a8866b4098fc10b8127f4125
iugin <eugenio.pierfederici@studio.unibo.it>
2018-07-07 09:26:51 +0200

    T6

--- scripts/upload_docs.sh
+++ scripts/upload_docs.sh
@@ -4,11 +4,7 @@ if [ -n "$GITHUB_API_KEY" ]
   [ "$TRAVIS_PULL_REQUEST" == "false" ] &&
   [ "$TRAVIS_BRANCH" == "master" ]; then
 
-  echo -e "Publishing docs...\n"
-  
-  ls client
-  pwd
-  
+  echo -e "Publishing docs:\n"
   mkdir $HOME/latest-javadoc
   mkdir $HOME/latest-scaladoc
   find -path "*/latest-docs/java" -print0 | while IFS= read -r -d $'\0' line; do ls $line; cp -r $line/* $HOME/latest-javadoc; done;
@@ -16,7 +12,7 @@ if [ -n "$GITHUB_API_KEY" ]
 	cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GITHUB_API_KEY}@github.com/iuginP/pps-17-cw-mp-tests gh-pages > /dev/null
+  git clone --quiet --branch=gh-pages https://${GITHUB_API_KEY}:@github.com/iuginP/pps-17-cw-mp-tests gh-pages > /dev/null
   cd gh-pages
   git rm -rf ./java
   git rm -rf ./scala
